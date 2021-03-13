59528272424dc2a46fcc7dc28dd9ebedbc70a951
TanveerSodhi <60098315+TanveerSodhi@users.noreply.github.com>
2021-03-14 00:10:53 +0530

    Some description added

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -38,7 +38,7 @@ class LandingPage extends Component {
         }
         console.log('opensignup', OpenSignup)
         return (
-            <div className="container" /*style={{backgroundImage:`url('/assets/images/ezgif.com-gif-maker.jpg')`,backgroundSize:'cover'}}*/>
+            <div className="container-fluid" style={{backgroundImage:`url('/assets/images/bg2.jpg')`,backgroundRepeat:'no-repeat',backgroundSize:'cover',height:'100%',overflow:'hidden',width:'100%'}}>
                 <div className="row landing" style={{paddingLeft:0,marginLeft:0}}   >
                     <div className="col-sm-5" style={{justifyContent:'left', }}>
                         <div className='row' style={{justifyContent:'center' }}>
@@ -49,9 +49,40 @@ class LandingPage extends Component {
                     <div className='col-sm-7' style={{display: "flex", justifyContent: "center", alignItems: "center"}}>{OpenSignup ? <SignUp onClick={this.handleSignIn}/> : <SignIn onClick={this.handleRegister}/>}</div>
                 </div>
                 <hr/>
-                <div className="row" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                {/*<div className="row" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                     <h1>Project Description</h1>
+                    <div>our project is a website based on a virtual trading platform which will provide
+budding investors a platform to dive into the trading of cryptocurrencies without
+having any fear of losing anything.</div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+
+        </div>*/}
+        <div className='container-fluid col-12 text-center'>
+                <h1 >Description</h1>
+                <hr />
+                <p className="content container-fluid">Our project is a website based on a virtual trading platform which will provide<br/>
+budding investors a platform to dive into the trading of cryptocurrencies without<br/>
+having any fear of losing anything.</p>
+            </div>
+            <div className="feature container-fluid col-12">
+                <h2 className="feature-heading">Features</h2>
+                <hr className="feature-line" />
+                <div className="row offset-1 col-10">
+                    <ul className="feature-content">
+                        <li>User authentication</li>
+                        <li>Virtual Trading</li>
+                        <li>Real-time price updation</li>
+                        <li>Price Prediction of Cryptocurrency</li>
+                        <li>Latest News</li>
+                        <li>User Support</li>
+                    </ul>
                 </div>
+            </div>
                 <hr/>
                 
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,21 +9,21 @@ const NavComp = (props) => {
   return (
     <div>
       <Navbar dark>
-        <NavbarBrand href="/" className="me-0"><img src='/assets/images/logoN.png'height='100px'/> </NavbarBrand>
+        <NavbarBrand href="/" className="me-0" style={{display:'inline-block'}}><img src='/assets/images/logoN.png'height='200px'/></NavbarBrand>
         <NavbarToggler style={{backgroundColor: '#131A6C',marginRight:'40px'}} onClick={toggleNavbar} className="me-0"/>
         <Collapse  isOpen={!collapsed} navbar>
           <Nav navbar>
             <NavItem>
-              <NavLink style={{color:'darkcyan'}} href="/aboutus/"><span color='violet' >About us</span></NavLink>
+              <NavLink style={{color:'darkcyan',fontSize:'1.5rem'}} className='regB' href="/aboutus/"><span color='violet' >About us</span></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color:'darkcyan'}} href="/contactus/">Contact Us </NavLink>
+              <NavLink style={{color:'darkcyan',fontSize:'1.5rem'}} className='regB' href="/contactus/">Contact Us </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color:'darkcyan'}} href="/Collaborators/">Collaborators </NavLink>
+              <NavLink style={{color:'darkcyan',fontSize:'1.5rem'}} className='regB' href="/Collaborators/">Collaborators </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink style={{color:'darkcyan'}} href="/faqs/">FAQs </NavLink>
+              <NavLink style={{color:'darkcyan',fontSize:'1.5rem'}} className='regB' href="/faqs/">FAQs </NavLink>
             </NavItem>
           </Nav>
         </Collapse>
