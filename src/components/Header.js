f2265a10fee16b469ca4103c0dc6f2a9c459608a
TanveerSodhi <60098315+TanveerSodhi@users.noreply.github.com>
2021-03-21 01:02:33 +0530

    installed charts and webSocket

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import NavComp from './Navbar';
 class Header extends Component {
     render() {
         return (
-            <div /*className="jumbotron jumbotron-fluid text-center"*/ style={{backgroundColor:'white',marginTop:'0px',marginBottom:'0px',paddingBottom:'0px'}}>
+            <div /*className="jumbotron jumbotron-fluid text-center"*/ style={{backgroundColor:'white',marginTop:'0px',marginBottom:'0px',paddingBottom:'0px',boxShadow:'0px 4px 4px rgba(0, 0, 0, 0.25)'}}>
                 
                 
                 {/* <h1 id='top' style={{marginTop:0,fontSize:'3rem'}} className='bg-text'>Compra Venta</h1> */}
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Header from './Header';
 import LandingPage from './LandingPage'
 import Footer from './Footer';
+import { LightweightChart } from './Chart';
 class MainComponent extends Component {
     render() {
         return (
             <div className="container-full-bg" >
               <Header/>  
               <LandingPage/>
+              {/*<LightweightChart/>*/}
               <Footer/>
             </div>
         );
