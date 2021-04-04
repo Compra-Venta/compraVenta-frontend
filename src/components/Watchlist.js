2618ba6d13112e401c3ae21a474fb4f1389c0b16
Aseem Mangla <60098357+manglaaseem28@users.noreply.github.com>
2021-04-04 19:44:40 +0530

    Warnings corrected

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -25,15 +25,15 @@ export class Watchlist extends Component {
         var binanceSocket= new WebSocket(socketUrl)
 	    binanceSocket.onmessage = (event)=>{
 		var res = JSON.parse(event.data)
-		console.log(res)
+		// console.log(res)
        res.filter((ticker)=> {
-            if(symbol.find(sym => sym == ticker.s))
+            if(symbol.find(sym => sym === ticker.s))
            {
             //    console.log(ticker.c)
             var prices = this.state.prices;
             var prev_price;
             // console.log(prices)
-            if(Object.keys(prices).find( (label, value) => label == ticker.s ))
+            if(Object.keys(prices).find( (label, value) => label === ticker.s ))
             prev_price = prices[ticker.s].price;
             else
             prev_price = 0;
@@ -51,7 +51,7 @@ export class Watchlist extends Component {
 
     check = () => {
         const ws  = this.state.ws;
-        if (ws || ws.readyState == WebSocket.OPEN) {
+        if (ws || ws.readyState === WebSocket.OPEN) {
             console.log('connection check');
         } 
     };
@@ -67,7 +67,7 @@ export class Watchlist extends Component {
 
         // console.log(prices);
         let watchlists = Object.keys(prices).map(( (label, value) => {
-            console.log(label, prices[label]);
+            // console.log(label, prices[label]);
             return (
                 <div className='row'>
                     <div className='col-4 col-md-4' style={{color: prices[label].color}} >
