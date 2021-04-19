92d641cd6e55a3ecdebbbf2b0f7c13899bfca23f
Aseem Mangla <60098357+manglaaseem28@users.noreply.github.com>
2021-04-20 01:50:56 +0530

    updated collaborators

--- src/components/Collaborators.js
+++ src/components/Collaborators.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-// import {
-//     faYoutube,
-//     faFacebook,
-//     faInstagram
-//   } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faLinkedin,
+    faChrome,
+    faGithub
+  } from "@fortawesome/free-brands-svg-icons";
 import NavDash from './NavDashboard'
 
 export class Collaborators extends Component {
@@ -14,27 +15,27 @@ export class Collaborators extends Component {
              data:[
                  {
                      name: 'Tanveer Sodhi',
-                     mail: 'aseemmangla.bt19ele@pec.edu.in',
-                     linkedln: 'https://www.linkedin.com/in/aseemmangla',
-                     insta: 'https://www.instagram.com/mangla_aseem/',
+                     mail: 'tanveersodhi.bt19cse@pec.edu.in',
+                     linkedln: 'https://www.linkedin.com/in/tanveer-sodhi-89329b190',
+                     github: 'https://github.com/TanveerSodhi',
                  },
                  {
                     name: 'Jaskaran Singh',
-                    mail: 'aseemmangla.bt19ele@pec.edu.in',
-                    linkedln: 'https://www.linkedin.com/in/aseemmangla',
-                    insta: 'https://www.instagram.com/mangla_aseem/',
+                    mail: 'jaskaransingh.bt19cse@pec.edu.in',
+                    linkedln: 'https://www.linkedin.com/in/jaskaran-singh-351426192/',
+                    github: 'https://github.com/jaskaran-23',
                 },
                 {
                     name: 'Daksh Verma',
-                    mail: 'aseemmangla.bt19ele@pec.edu.in',
-                    linkedln: 'https://www.linkedin.com/in/aseemmangla',
-                    insta: 'https://www.instagram.com/mangla_aseem/',
+                    mail: 'dakshverma.bt19cse@pec.edu.in',
+                    linkedln: 'https://www.linkedin.com/in/daksh-verma/',
+                    github: 'https://github.com/dakshverma2411/',
                 },
                 {
                     name: 'Aseem Mangla',
                     mail: 'aseemmangla.bt19ele@pec.edu.in',
                     linkedln: 'https://www.linkedin.com/in/aseemmangla',
-                    insta: 'https://www.instagram.com/mangla_aseem/',
+                    github: 'https://github.com/manglaaseem28',
                 }
              ]
         }
@@ -43,26 +44,87 @@ export class Collaborators extends Component {
     render() {
         const AseemMangla = 'Aseem Mangla'
         const imgUrl = `assets/images/collaborators/${AseemMangla}.png`
+        const collaboratorsData = this.state.data.map( person => {
+            return(
+                    <div className='column' id='collaborator'>
+                    <div className='card'>
+                        <img className='collaborator-pic' src={`assets/images/collaborators/${person.name}.png`} />
+                        <div className='handlebox'>
+                        <div className='socialhandles' >
+                            <a href={`mailto:${person.mail}`} target="_blank">
+                                <FontAwesomeIcon icon={faChrome} size="4x" />
+                            </a>
+                            <a href={`${person.linkedln}`} target="_blank">
+                                <FontAwesomeIcon icon={faLinkedin} size="4x" />
+                            </a>
+                            <a href={`${person.github}`} target="_blank">
+                                <FontAwesomeIcon icon={faGithub} size="4x" />
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    <div className='colab-name'>
+                        <h2>{`${person.name}`}</h2>
+                    </div>
+                    </div>
+            )
+        } )
         return (
             <div>
                 <div>
                     <NavDash/>
                 </div>
-                <div 
-                style={{
-                    
-                    backgroundImage: `url("${imgUrl}")`,
-                    width: '400px',
-                    height: '500px'
-                    }}>
-                        hihh
-                        
+                <div className='row'>
+                    {collaboratorsData}
                 </div>
-                <img src={`assets/images/collaborators/` + `Aseem Mangla` + `.png`} width='250px'/>
-
             </div>
         )
     }
 }
 
 export default Collaborators
+
+/*
+<h1>
+  <img src="../../assets/website team/website-team.png" class="heading" />
+</h1>
+<div class="d-flex team-grid mx-5">
+  <div
+    *ngFor="let member of websiteTeam; index as i"
+    class="image rounded card border-0 shadow"
+  >
+    <img
+      *ngIf="member.event == true"
+      src="..//..//assets/website team/{{ member.gif }}"
+      class="card-img-top"
+      alt="{{ member.name }}"
+    />
+    <img
+      src="..//..//assets/website team/{{ member.image }}"
+      class="card-img-top"
+      alt="{{ member.name }}"
+    />
+    <div class="overlay d-flex">
+      <a *ngIf="member.fb" class="fa fa-facebook" href="{{ member.fb }}"></a>
+      <a
+        *ngIf="member.linked"
+        class="fa fa-linkedin"
+        href="{{ member.linked }}"
+      ></a>
+      <a
+        *ngIf="member.insta"
+        class="fa fa-instagram"
+        href="{{ member.insta }}"
+      ></a>
+    </div>
+    <div class="card-body text-center">
+      <h4>
+        {{ member.name }}
+      </h4>
+      <p>
+        <b>{{ member.post }}</b>
+      </p>
+    </div>
+  </div>
+</div>
+*/
