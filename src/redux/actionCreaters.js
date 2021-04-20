41d23f43ba3e12cffa1443ca2f5d3bc18252f450
TanveerSodhi <60098315+TanveerSodhi@users.noreply.github.com>
2021-04-20 13:55:26 +0530

    registration redux

--- src/redux/actionCreaters.js
+++ src/redux/actionCreaters.js
@@ -24,8 +24,8 @@ export const requestLogin = (creds) => {
 export const receiveLogin = (response) => {
     return {
         type: ActionTypes.LOGIN_SUCCESS,
-        token: response.token,
-        admin: response.admin
+        token: response.access_token,
+        
     }
 }
 
@@ -63,7 +63,7 @@ export const loginUser = (creds) => (dispatch) => {
         .then(response => {
             if (response.success) {
                 // If login was successful, set the token in local storage
-                localStorage.setItem('token', response.token);
+                localStorage.setItem('token', response.access_token);
                 localStorage.setItem('creds', JSON.stringify(creds));
                 
                 // Dispatch the success action
@@ -96,7 +96,72 @@ export const logoutUser = () => (dispatch) => {
     dispatch(requestLogout())
     localStorage.removeItem('token');
     localStorage.removeItem('creds');
-    localStorage.removeItem('admin');
+    
    
     dispatch(receiveLogout())
 }
+
+export const requestRegister = (creds) => {
+    return {
+        type: ActionTypes.REGISTER_REQUEST,
+        creds
+    }
+}
+
+export const receiveRegister = () => {
+    return {
+        type: ActionTypes.REGISTER_SUCCESS,
+        
+        
+    }
+}
+
+export const registerError = (message) => {
+    return {
+        type: ActionTypes.REGISTER_FAILURE,
+        message
+    }
+}
+
+export const RegisterUser = (creds) => (dispatch) => {
+    // We dispatch requestRegister to kickoff the call to the API
+    dispatch(requestRegister(creds))
+
+    return fetch(baseUrl + '/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(creds)
+    })
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                throw error;
+            })
+        .then(response => response.json())
+        .then(response => {
+            if (response.success) {
+                // If login was successful, set the token in local storage
+                
+                localStorage.setItem('creds', JSON.stringify(creds));
+                
+                // Dispatch the success action
+                
+                dispatch(receiveRegister());
+            }
+            else {
+                var error = new Error('Error ' + response.status);
+                error.response = response;
+                throw error;
+            }
+        })
+        .catch(error => dispatch(registerError(error.message)))
+};
