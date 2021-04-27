36e8ba27fc05d879f932d4a898b1bcf59f794a08
TanveerSodhi <60098315+TanveerSodhi@users.noreply.github.com>
2021-04-27 22:47:52 +0530

    updated learn

--- src/components/LearnDash.js
+++ src/components/LearnDash.js
@@ -9,16 +9,16 @@ const LearnDash = (props) => {
 
   return (
     <div style={{boxShadow:'0px 4px 4px rgba(0, 0, 0, 0.25)',backgroundColor:'black'}}>
-      <Navbar light='false' expand='md' /*style={{marginBottom:'0px',marginTop:'0px',borderWidth:'medium'}}*/ className=' mr-0' style={{ padding:'0px'}} >
+      <Navbar light='false' expand='md' /*style={{marginBottom:'0px',marginTop:'0px',borderWidth:'medium'}}*/ className=' mr-0 fixed-nav-bar' style={{ padding:'0px'}} >
         {/*{<div className='container'> }*/}
         <NavbarBrand  href="/" className="mr-auto" ><div className='col-sm-2'><img src='/assets/images/HeadLogoLearn.png'height='40vh' width='40vh' style={{display:'inline-block',float:'left',maxHeight:'100%',width:'4vw',height:'4vw'}} /> </div><div style={{width:'100%',textAlign:'center',justifyContent:'center',verticalAlign:'center',marginTop:'auto'}} className='col-sm-3'> <h1 id='top' style={{fontSize:'3vw',alignContent:'center',color:'gold'}}>Compra Venta</h1></div></NavbarBrand>
         <NavbarToggler style={{backgroundColor: 'white'}} onClick={toggleNavbar} className="me-0"/>
         <Collapse  isOpen={!collapsed} navbar>
           <Nav className='ml-auto col-md-7 justify-content-end' navbar style={{paddingRight:'0px',justifyItems:'right'}} >
-            <NavItem>
+            <NavItem className='my-auto'>
               <NavLink style={{color:'white',fontSize:'1.4rem',paddingRight:'2vw'}} href="/collaborators">Collaborators</NavLink>
             </NavItem>
-            <NavItem>
+            <NavItem className='my-auto'>
               <NavLink style={{color:'white',fontSize:'1.4rem',paddingRight:'2vw'}}  href="/faqs">FAQs </NavLink>
             </NavItem>
             <div className='getStarted text-center' style={{justifyContent:'center',justifyItems:'center'}}>
@@ -26,7 +26,7 @@ const LearnDash = (props) => {
               <NavLink style={{color:'white',fontSize:'1.4rem',paddingRight:'1vw'}}  href="/home">Get Started</NavLink>
             </NavItem>
             </div>
-            <NavItem>
+            <NavItem className='my-auto'>
               <NavLink style={{color:'white',fontSize:'1.4rem',paddingRight:'2vw',paddingLeft:'1.6vw'}}  href="/aboutus">About Us </NavLink>
             </NavItem>
           </Nav>
